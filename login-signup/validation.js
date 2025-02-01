3d06caee722e5c2bbd01114512beaee49e607fbe
Vanflame <maligayasda@gmail.com>
2025-02-01 23:00:26 +0800

    Loginjs

--- login-signup/validation.js
+++ login-signup/validation.js
@@ -1,8 +1,8 @@
 const form = document.getElementById('form')
 const firstname_input = document.getElementById('firstname-input')
-const email_input = document.getElementById('email-input')
-const password_input = document.getElementById('password-input')
-const repeat_password_input = document.getElementById('repeat-password-input')
+const email = document.getElementById('email-input')
+const password = document.getElementById('password-input')
+const repeat_password = document.getElementById('repeat-password-input')
 const error_message = document.getElementById('error-message')
 
 form.addEventListener('submit', (e) => {
@@ -10,11 +10,11 @@ form.addEventListener('submit', (e) => {
 
   if(firstname_input){
     // If we have a firstname input then we are in the signup
-    errors = getSignupFormErrors(firstname_input.value, email_input.value, password_input.value, repeat_password_input.value)
+    errors = getSignupFormErrors(firstname_input.value, email.value, password.value, repeat_password.value)
   }
   else{
     // If we don't have a firstname input then we are in the login
-    errors = getLoginFormErrors(email_input.value, password_input.value)
+    errors = getLoginFormErrors(email.value, password.value)
   }
 
   if(errors.length > 0){
@@ -33,20 +33,20 @@ function getSignupFormErrors(firstname, email, password, repeatPassword){
   }
   if(email === '' || email == null){
     errors.push('Email is required')
-    email_input.parentElement.classList.add('incorrect')
+    email.parentElement.classList.add('incorrect')
   }
   if(password === '' || password == null){
     errors.push('Password is required')
-    password_input.parentElement.classList.add('incorrect')
+    password.parentElement.classList.add('incorrect')
   }
   if(password.length < 8){
     errors.push('Password must have at least 8 characters')
-    password_input.parentElement.classList.add('incorrect')
+    password.parentElement.classList.add('incorrect')
   }
   if(password !== repeatPassword){
     errors.push('Password does not match repeated password')
-    password_input.parentElement.classList.add('incorrect')
-    repeat_password_input.parentElement.classList.add('incorrect')
+    password.parentElement.classList.add('incorrect')
+    repeat_password.parentElement.classList.add('incorrect')
   }
 
 
@@ -58,17 +58,17 @@ function getLoginFormErrors(email, password){
 
   if(email === '' || email == null){
     errors.push('Email is required')
-    email_input.parentElement.classList.add('incorrect')
+    email.parentElement.classList.add('incorrect')
   }
   if(password === '' || password == null){
     errors.push('Password is required')
-    password_input.parentElement.classList.add('incorrect')
+    password.parentElement.classList.add('incorrect')
   }
 
   return errors;
 }
 
-const allInputs = [firstname_input, email_input, password_input, repeat_password_input].filter(input => input != null)
+const allInputs = [firstname_input, email, password, repeat_password].filter(input => input != null)
 
 allInputs.forEach(input => {
   input.addEventListener('input', () => {
@@ -78,5 +78,38 @@ allInputs.forEach(input => {
     }
   })
 })
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyC3HObihU08C4n8whNvxEalbmYxOJiubwQ",
+    authDomain: "login-database-6d587.firebaseapp.com",
+    databaseURL: "https://login-database-6d587-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "login-database-6d587",
+    storageBucket: "login-database-6d587.firebasestorage.app",
+    messagingSenderId: "190425522088",
+    appId: "1:190425522088:web:ba6985fe4f8cf10f8b879d",
+    measurementId: "G-C7JHZMK3BM"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+
+ 
+
+  //submit
+  const submit = document.getElementById('submit').value;
+  submit.addEventListener("click", function(event){
+    event.preventDefault()
+    alert(1)
+  })
 
  
